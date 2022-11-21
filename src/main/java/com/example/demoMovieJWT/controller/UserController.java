788bee04bb8b9c903ad5d535146192c25ff6f0de
Nilufa Khan <khannilufa5@gmail.com>
2022-11-21 10:49:19 +0530

    Initial commit

--- src/main/java/com/example/demoMovieJWT/controller/UserController.java
+++ src/main/java/com/example/demoMovieJWT/controller/UserController.java
@@ -12,13 +12,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+
 
 @RestController
 public class UserController {
     UserService userService;
     SecurityTokenGenerate securityTokenGenerate;
+
 
     @Autowired
     public  UserController(UserService userService, SecurityTokenGenerate securityTokenGenerate){
